caf2f8bed775ed54d88ffd7e5a4f73db981e82d9
Gonzalo <gonzalo.fce@gmail.com>
2021-02-02 11:01:00 +0100

    add solution to Polyposis model: Day 1

--- contraceptive_use.R
+++ contraceptive_use.R
@@ -81,3 +81,76 @@ anovatab <- data.frame(model = rhs,
                        df = unlist(lapply(models,df.residual)) )
 
 anovatab
+
+
+
+#### Clinical Trial on Polyposis ####
+
+polyps <- read.table("polyps.txt", sep="", header=T)
+
+glimpse(polyps)
+
+
+###### Solving problem with Null model ######
+m0 <- glm(number ~ 1, data = polyps, family = poisson(link=log))
+
+# We estimate a model log-linear model with only a mean, we thus estimated the log of the mean number of polyps:
+m0$coef
+log(sum(polyps$number)/nrow(polyps))
+
+
+###### Solving problem with 1 var ######
+mA <- glm(number ~ age, data = polyps, family = poisson(link=log))
+summary(mA)
+
+# An additional year in age will result to a decrease in the number of polyps equal to about 3%
+# $ 1 - \exp{`r mA$mA$coefficients[2]`} = 0.031 $
+
+# Calculation with some fitted values for age 20 and 21
+pA <- predict(mA, data.frame(age=c(20, 21)), type="response")
+pA
+# The estimated coefficient inform about the (constant) difference in the logarithm of the estimated polyps
+
+
+###### Solving problem with complete model ######
+mAT <- glm(number ~ age + treatment + age*treatment, data = polyps, family = poisson(link=log))
+summary(mAT)
+
+anova(mAT)
+# No interaction is needed
+
+mAT <- glm(number ~ age + treatment, data = polyps, family = poisson(link=log))
+summary(mAT)
+
+# A final plot could help to check the outcomes of the model, and we do in a log-scale to appreciate the
+# linearity and the parallelism between the placebo and treated group:
+ages <- seq(min(polyps$age), max(polyps$age), length=100)
+mydf <- expand.grid(age=ages, treatment=c(0,1))
+y.hat <- matrix(predict(mAT, newdata = mydf),100)
+plot(polyps$age, log(polyps$number), col=polyps$treatment+1, xlab="age", ylab="number of polyps, log-scale")
+matlines(ages, y.hat, col=1:2, lty=1)
+legend("topright", c("Placebo", "Treatment"), col=1:2, lty=1, pch=1)
+
+
+
+###### Solving problem with aggregate data ######
+
+mytab <- table(polyps$age, polyps$treatment)
+mydf <- expand.grid(age=as.numeric(dimnames(mytab)[[1]]),
+                    treat=as.numeric(dimnames(mytab)[[2]]))
+mydf$e <- c(mytab)
+
+sumnum <- tapply(polyps$number, list(polyps$age, polyps$treatment), sum)
+mydf$y <- as.vector(sumnum)
+mydf <- mydf[mydf$e!=0, ]
+
+# The dataframe mydf contains the same information of the original dataset and fitting the same model will lead to equal outcomes
+# OFFSET!! is necessary when using aggregate data
+mATagg <- glm(y~age+treat, offset=log(e), family=poisson(), data=mydf)
+mATagg$coef
+# vs.
+mAT$coef
+
+
+
+
