45d076e1f962acd94c972bf72b9b45a29e28f5d4
Gonzalo <gonzalo.fce@gmail.com>
2021-02-02 16:59:01 +0100

    edit plot Q1 + center type data

--- Event_History_Camarda_exercise_4.R
+++ Event_History_Camarda_exercise_4.R
@@ -1,39 +1,45 @@
 #### Importing packages ####
 library(tidyverse)
-
+library(wesanderson)
 
 #### 1. Load the data and quickly present a possible research questions with the help of descriptive statistics ####
-cancer <- read.table("cancer.txt",header=TRUE)
+cancer <- read.table("CancerBishopEtAl1975.txt", header=TRUE, sep=",")
 
 glimpse(cancer)
 
+
 # Create Survivor % rate for each group
-# Pivot_wider to have 2 columns for malignant dimension
+# Colors by type of Center, split into 2 by type of tumor
+# Calculate Avg Rate of Survival between type of tumor and add dashed line to facet_wrap
+# Use Wes Anderson's palette for filling the bars
+
 cancer %>% 
   mutate(survivor = yes/(yes+no)) %>%
-  select(-c(yes,no)) %>% 
-  pivot_wider(names_from=malignant, names_prefix="malignant_", values_from=survivor) -> cancer2
+  group_by(Malignant) %>% 
+  summarise(MN = mean(survivor)) -> mean_mal
 
-#Create a Matrix which will help in creating the plot
-value_matrix = matrix(, nrow = 2, ncol = 3)
-
-#An empty matrix is a necessary requirement prior to copying data
-value_matrix[1,] = cancer2$malignant_no 
-value_matrix[2,] = cancer2$malignant_yes
-
-# Note that the "beside" argument has to be kept "TRUE" in order to place the bars side by side
-barplot(value_matrix, names.arg = cancer2$age, beside = TRUE, col = c("peachpuff", "skyblue"), legend.text = c("No malignant", "Malignant"))
+cancer %>% 
+  mutate(survivor = yes/(yes+no)) %>% 
+  ggplot(aes(x=age, y = survivor, fill = center)) + geom_bar(position="dodge", stat="identity") +
+  geom_hline(data = mean_mal, aes(yintercept = MN), linetype="dashed", color = "black", size=1.1) +
+  facet_wrap(~ Malignant) +
+  scale_fill_manual(values=wes_palette(n=3, name="Darjeeling1"))
 
 
-# From plot we see that Survivor seems to be negatively correlated to both Age and the "malignity" of the tumor
-# We'll try using both dimensions to model the survivorship: YES vs NO => logistic
+# We have some mixed results:
+# a1) When Tumor is NO malignant, age and survivor are negatively correlated
+# a2) When Tumor is YES malignant, we have mixed results: I believe there is another dimension we are missing
+# b) Centers have different survivor rates. Tokyo is always better...
+# c) Type of Tumor is definitely something to take into account (dashed line)
+
+# We'll try using all dimensions to model the survivorship: YES vs NO => logistic
 
 
 #### 2. Model these data to answer the question (do not use any interaction) ####
 
 # I'm taking out the intercept so reading the betas is easier
 # The base category is NO malignant tumor
-m.cancer <- glm(cbind(yes,no) ~ age + malignant - 1, family=binomial(link=logit), data=cancer)
+m.cancer <- glm(cbind(yes,no) ~ age + Malignant + center - 1, family=binomial(link=logit), data=cancer)
 summary(m.cancer)
 
 # All p-values below 5%. GOOD
@@ -46,10 +52,11 @@ summary(m.cancer)
 # Using AIC to compare between the models: the lower the better
 
 m.cancer.age <- glm(cbind(yes,no) ~ age - 1, family=binomial(link=logit), data=cancer)
-m.cancer.mal <- glm(cbind(yes,no) ~ malignant - 1, family=binomial(link=logit), data=cancer)
+m.cancer.mal <- glm(cbind(yes,no) ~ Malignant - 1, family=binomial(link=logit), data=cancer)
+m.cancer.center <- glm(cbind(yes,no) ~ center - 1, family=binomial(link=logit), data=cancer)
 
 
-AIC(m.cancer.age, m.cancer.mal, m.cancer)
+AIC(m.cancer.age, m.cancer.mal, m.cancer.center, m.cancer)
 # The complete model performs better
 
 
