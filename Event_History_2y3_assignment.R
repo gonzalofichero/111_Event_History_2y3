8b86fde4746afb27404a602bdf011ebd4014e798
Gonzalo <gonzalo.fce@gmail.com>
2021-02-10 09:52:24 +0100

    create Assigment Exercise 1

---
+++ Event_History_2y3_assignment.R
@@ -0,0 +1,55 @@
+#### Loading libraries ####
+library(tidyverse)
+library(survival)
+
+
+#### Importing data ####
+breast <- read.table("breast.txt")
+
+glimpse(breast)
+
+
+#### Q1. Estimate a proportional hazard model with a Weibull distribution as baseline. ####
+
+# Use patient with group=Good as reference
+breast %>% 
+  mutate(x1 = as.factor(ifelse(group == "Medium", 1, 0)),
+         x2 = as.factor(ifelse(group == "Poor", 1, 0))) -> breast
+
+# Generating a logL for Weibull: 2 parameters + 2 covariates (binary)
+
+# Loglikelihood function with right-censoring
+logL.W <- function(para, entry, exit, observation,x1,x2){
+  ## Weibull parameters
+  a <- par[1]
+  b <- par[2]
+  ## regression coefficients
+  beta <- par[3:4]
+  ## design matrix
+  X <- cbind(x1, x2)
+  ## regression part
+  Xbeta <- X%*%beta
+  expXbeta <- exp(Xbeta)
+  
+  ## In right-censoring we have the following LogL:
+  # \sum_{i=1}^{n} (\delta_i * log(h(y_i)) - H(y_i))
+  # here we have the event in the exact different position: so, we change delta to (1-delta)
+  
+  
+  a <- para[1]
+  b <- para[2]
+  f1 <- para[3]
+  f2 <- para[4]
+  
+  logL <- sum(log(a)-log(observation)+a*log(observation)-a*log(b)+f1*x1+f2*x2)
+  
+  return(logL)
+}
+
+# Setting initial parameters
+ini_par <- c(1,10,1,1)
+
+# Optimization
+opt.weibull <- optim()
+
+
